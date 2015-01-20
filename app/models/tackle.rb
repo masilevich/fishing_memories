2a1f036ca5573204b73a68f3366aac385a6539e8
Masilevich Leanid <l.masilevich@gmail.com>
2015-01-20 17:15:07 +0300

    add related to ponds and tackle sets

--- app/models/tackle.rb
+++ app/models/tackle.rb
@@ -3,4 +3,10 @@ class Tackle < ActiveRecord::Base
 
 	has_and_belongs_to_many :memories
 	has_and_belongs_to_many :tackle_sets
+
+	 def memories_with_tackle_sets_memories
+    joins(:tackle_sets).joins(:memories)
+    #memories.joins(:products).where(:products => {:brand_id => 1})
+    #memories.joins("join pages_paragraphs on pages.id = pages_paragraphs.page_id").where(["pages_paragraphs.paragraph_id = ?", paragraph_id])
+  end
 end
