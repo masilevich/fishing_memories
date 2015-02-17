8dbcdd51467f1e1bb99c4c7076fea7ec01e80655
Masilevich Leanid <l.masilevich@gmail.com>
2015-02-17 20:07:55 +0300

    Add precompiled assets for Heroku

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,3 +8,5 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( search.js )
 
 Rails.application.config.assets.precompile += %w( ckeditor/* )
+
+Rails.application.config.assets.precompile += ["select2/*.png", "select2/*.gif"]
