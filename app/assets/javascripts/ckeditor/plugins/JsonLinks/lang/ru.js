2af2205c8e1a3d09961fae29be1423f4b6b64f87
Masilevich Leanid <l.masilevich@gmail.com>
2015-04-15 16:37:23 +0300

    Add precompiled assets for Heroku

--- app/assets/javascripts/ckeditor/plugins/JsonLinks/lang/ru.js
+++
@@ -1,7 +0,0 @@
-﻿CKEDITOR.plugins.setLang( 'JsonLinks', 'ru', {
-	lures: {
-		button_label: 'Вставить ссылку на приманку',
-		select_label: 'Приманки',
-		dialog_title: "Выберите приманку"
-	}
-} );
