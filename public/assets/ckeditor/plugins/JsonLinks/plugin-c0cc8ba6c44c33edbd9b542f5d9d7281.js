eefb6c7d4851db830a65d0fc2b6e9863540a17ad
Masilevich Leanid <l.masilevich@gmail.com>
2015-04-16 09:17:03 +0300

    Add precompiled assets for Heroku

--- public/assets/ckeditor/plugins/JsonLinks/plugin-c0cc8ba6c44c33edbd9b542f5d9d7281.js
+++
@@ -1,1 +0,0 @@
-CKEDITOR.plugins.add("JsonLinks",{icons:"jsonlinks",lang:["ru"],init:function(n){n.addCommand("jsonlinks",new CKEDITOR.dialogCommand("jsonlinksDialog")),n.ui.addButton("JsonLinks",{label:n.lang.JsonLinks.lures.button_label,icons:"jsonlinks",command:"jsonlinks"})}});
