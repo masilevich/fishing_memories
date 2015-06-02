c11d1891dd84a60755ad4d6cfdf491a9153da0bd
Masilevich Leanid <l.masilevich@gmail.com>
2015-06-02 14:43:04 +0300

    maps show points

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -2,4 +2,16 @@ class MapsController < ApplicationController
 
 	load_and_authorize_resource
 	
+	def show
+		@map = Map.find(params[:id])
+		@points = @map.try(:points)
+		@map_id = @map.id
+		if @points.try(:any?)
+	    @json = @points.to_gmaps4rails do |point, marker|
+	      @point = point
+	      marker.infowindow render_to_string('points/show', :layout => false)    
+	      marker.json({ :id => @point.id })
+	    end
+  	end
+	end
 end
